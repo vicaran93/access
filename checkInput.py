ec7b9c35224e3e03418ebcc16b81a2ba8887a934
Victor Arango <varangoquiro@umass.edu>
2017-11-16 18:35:42 -0500

    checkinput.py check now input for pin 4,17, and 27

--- checkInput.py
+++ checkInput.py
@@ -1,16 +1,22 @@
 import RPi.GPIO as GPIO
 
 #This method uses pin 18 to check its input; 10 times every second
-
+#initialize input pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18,GPIO.IN)
+GPIO.setup(4,GPIO.IN)
+GPIO.setup(17,GPIO.IN)
+GPIO.setup(27,GPIO.IN)
 
-x = GPIO.input(18)
-
-print(x)
+#x = GPIO.input(18)
+#print(x)
 
 while GPIO.input(18) == 0:
-	print("not pressed yet")
-	
-print("button was pressed")
+	if GPIO.input(4) == 1: #pressed
+		print("PIN4 pressed")
+	if GPIO.input(17) == 1: #pressed
+		print("PIN17 pressed")
+	if GPIO.input(27) == 1: #pressed
+		print("PIN27 pressed")
+print("PIN18 was pressed")
 GPIO.cleanup()

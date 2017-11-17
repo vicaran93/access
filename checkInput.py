dd8ab5c6c686eb0a916bf9ddf40283e712187080
Victor Arango <varangoquiro@umass.edu>
2017-11-16 19:11:55 -0500

    improved checkInput.py by excuting only once a line of code when a button is pressed.

--- checkInput.py
+++ checkInput.py
@@ -1,22 +1,54 @@
 import RPi.GPIO as GPIO
 
-#This method uses pin 18 to check its input; 10 times every second
-#initialize input pins
+# This method uses pin 18 to check its input; 10 times every second
+# initialize input pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.IN)
-GPIO.setup(4,GPIO.IN)
-GPIO.setup(17,GPIO.IN)
-GPIO.setup(27,GPIO.IN)
+GPIO.setup(18, GPIO.IN)
+GPIO.setup(4, GPIO.IN)
+GPIO.setup(17, GPIO.IN)
+GPIO.setup(27, GPIO.IN)
 
-#x = GPIO.input(18)
-#print(x)
+# x = GPIO.input(18)
+# print(x)
+
+once = True
+pin4_pressed = False
+pin17_pressed = False
+pin27_pressed = False
 
 while GPIO.input(18) == 0:
-	if GPIO.input(4) == 1: #pressed
-		print("PIN4 pressed")
-	if GPIO.input(17) == 1: #pressed
-		print("PIN17 pressed")
-	if GPIO.input(27) == 1: #pressed
-		print("PIN27 pressed")
+    if GPIO.input(4) == 1:  # pressed
+        if pin17_pressed or pin27_pressed:
+            # need to reset once to True
+            once = True
+            pin17_pressed = False
+            pin27_pressed = False
+        if once:
+            print("PIN4 pressed")
+            once = False
+            pin4_pressed = True
+
+    if GPIO.input(17) == 1:  # pressed
+        if pin4_pressed or pin27_pressed:
+            # need to reset once to True
+            once = True
+            pin4_pressed = False
+            pin27_pressed = False
+        if once:
+            print("PIN17 pressed")
+            once = False
+            pin17_pressed = True
+
+    if GPIO.input(27) == 1:  # pressed
+        if pin4_pressed or  pin17_pressed:
+            # need to reset once to True
+            once = True
+            pin4_pressed = False
+            pin17_pressed = False
+        if once:
+            print("PIN27 pressed")
+            once = False
+            pin27_pressed = True
+
 print("PIN18 was pressed")
 GPIO.cleanup()

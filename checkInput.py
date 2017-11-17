5aadf825d6c7cd266f92f42646e0fa4cb407f774
vicaran93 <varangoquiro@umass.edu>
2017-11-17 16:11:12 -0500

    change checkinput.py with new flag system that works better. Now we can press multiple times a button

--- checkInput.py
+++ checkInput.py
@@ -18,42 +18,31 @@ pin4_pressed = False
 pin17_pressed = False
 pin27_pressed = False
 
+flag=1 # do not init to 1 or 0
+
 while GPIO.input(18) == 0:
-    if GPIO.input(4) == 1:  # pressed
-        if pin17_pressed or pin27_pressed:
-            # need to reset once to True
-            once = True
-            pin17_pressed = False
-            pin27_pressed = False
+    if GPIO.input(4) == 1:
         if once:
             print("PIN4 pressed. Running showGreen.py")
 	    os.system('python showGreen.py')
             once = False
-            pin4_pressed = True
+            flag = 0
 
     if GPIO.input(17) == 1:  # pressed
-        if pin4_pressed or pin27_pressed:
-            # need to reset once to True
-            once = True
-            pin4_pressed = False
-            pin27_pressed = False
         if once:
             print("PIN17 pressed. Running showRed.py")
 	    os.system('python showRed.py')
             once = False
-            pin17_pressed = True
+            flag = 0
 
     if GPIO.input(27) == 1:  # pressed
-        if pin4_pressed or  pin17_pressed:
-            # need to reset once to True
-            once = True
-            pin4_pressed = False
-            pin17_pressed = False
         if once:
             print("PIN27 pressed. Taking picture...")
 	    os.system('./takepic.sh')
             once = False
-            pin27_pressed = True
 
+    if flag == 0:
+            once = True
+            flag = 1
 print("PIN18 was pressed. Program finished.")
 GPIO.cleanup()

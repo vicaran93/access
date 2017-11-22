6c1546e283109e2bbdb37555eccbf75fa153342b
Victor Arango <varangoquiro@umass.edu>
2017-11-22 14:49:54 -0500

    Improved compare.py to send cropped test img in case input name is not detected. Also fixed string comparisson

--- compare.py
+++ compare.py
@@ -13,7 +13,7 @@ if len(sys.argv) == 2:
     print("Seding: "+image_name)
 else:
     print("No input detected in compare.py...using test1.jpg as image name")
-    image_name="test1.jpg"
+    image_name="test1_cropped.jpg" #using test1.jpg does not work due to large image
 final_path=path+image_name
 # Load image:
 #img = Image.open(final_path)
@@ -25,10 +25,10 @@ final_url=base_url+"/compare.php"
 with open(final_path, 'rb') as f: response = requests.post(final_url, files={'userfile': f})
 
 print(response.text) #TEXT/HTML
-if response.text[0] is "I": # Image received...
+if response.text[0] == "I": # Image received...
     print("We got response back. Show green LED!")
     os.system('python showGreen.py')
-elif response.text[0] is "E": # Eror detected...
-    print("Got an error. Show red LED!")
+elif response.text[0] == "E": # Eror detected...
+    print("We got an error as a response. Show red LED!")
     os.system('python showRed.py')
 print(response.status_code, response.reason) #HTTP

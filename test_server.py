3919aaadd5fb92e42c4e4758290ca509c70ad12f
Victor Arango <varangoquiro@umass.edu>
2017-12-07 19:58:33 -0500

    test server working

--- test_server.py
+++ test_server.py
@@ -3,8 +3,20 @@ import sys,os
 
 
 
-final_url = ""
+final_url = "https://sdp-lh18.herokuapp.com/connection_test.php"
 
-content = "Test"
-
+content = {'test_var': 'test1'}
 response = requests.post(final_url, data=content)
+
+if response.text[0] == "S":
+    print("Test connection to Heroku and S3 database working")
+    # os.system('python showGreen.py')
+else:
+    print("Error!! No server connection")
+    # os.system('python showRed.py')
+
+
+
+
+print(response.status_code, response.reason)  # HTTP
+

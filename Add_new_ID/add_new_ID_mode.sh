40a9c4d4fef35bf9c0dcd51d89402e113d4fb17a
Victor Arango <varangoquiro@umass.edu>
2018-02-25 18:04:50 -0500

    fix 2

--- Add_new_ID/add_new_ID_mode.sh
+++ Add_new_ID/add_new_ID_mode.sh
@@ -10,12 +10,12 @@ DATE=$(date  +%Y-%m-%d_%H%M)
 echo "Picture name: $DATE"
 #raspistill -vf -q 100 -o ~/Documents/access/camera/$DATE.jpg
 #raspistill -ISO 400 -ss 160000 -br 80 -co 100 -vf -o ~/Documents/access/camera/$DATE.jpg
-python LED_and_pic.py $DATE
+python ./LEDs/LED_and_pic.py $DATE
 
 
 
 # Call crop.py  to crop image that we just took
-python crop.py $DATE
+python ./Image_processing/crop.py $DATE
 
 #$temp='_cropped'
 #$name=$DATE$temp
@@ -25,18 +25,18 @@ DATE+=$'_cropped'
 
 #python img2bw.py $DATE
 #python image_processing.py $DATE
-python ip_color_segmentation.py $DATE
+python ./Image_processing/ip_color_segmentation.py $DATE
 
 DATE+=$'_bw'
 
 # Call add_ID.py  to upload image to the server
-python add_ID.py $DATE
+python ./Add_new_ID/add_ID.py $DATE
 
 # Delete all files 
 #rm $DATE #remove black and white image
-DATE=${DATE%???}; # remove last three characters (_bw)
+#DATE=${DATE%???}; # remove last three characters (_bw)
 #rm $DATE #remove cropped image
-DATE=${DATE%????????}; # remove last 8  characters (_cropped)
+#DATE=${DATE%????????}; # remove last 8  characters (_cropped)
 #rm $DATE #remove original image
 
 
--- Compare_ID/compare_mode.sh
+++ Compare_ID/compare_mode.sh
@@ -15,39 +15,38 @@ DATE=$(date  +%Y-%m-%d_%H%M)
 #echo "out" > /sys/class/gpio/gpio21/direction
 #echo "1" > /sys/class/gpio/gpio21/value
 #raspistill -vf -o ~/Documents/access/camera/$DATE.jpg
-python ../LEDs/LED_and_pic.py $DATE
+python ./LEDs/LED_and_pic.py $DATE # From location of main.py
 #raspistill -ISO 400 -ss 160000 -br 80 -co 100 -vf -o ~/Documents/access/camera/$DATE.jpg
 #turn off UV LEDs
 #echo "21" > /sys/class/gpio/unexport
 
 # Call crop.py  to crop image that we just took
-python ../Image_processing/crop.py $DATE
+python ./Image_processing/crop.py $DATE
 
 DATE+=$'_cropped'
 
 #python img2bw.py $DATE # This does not deal with the artifacts encountered in the imgs
 #python image_processing.py $DATE
-python ../Image_processing/ip_color_segmentation.py $DATE
+python ./Image_processing/ip_color_segmentation.py $DATE
 
 DATE+=$'_bw'
 
-python ../Image_processing/better_template.py $DATE #Hassaan code
+python ./Image_processing/better_template.py $DATE #Hassaan code
 
 NAME='template' # Hassaan's code saves template taken from $DATE as  template.png
 
 # Call add_ID.py  to upload TEMPLATE to the server and then to compare
-python ../Add_new_ID/add_ID.py $NAME
-python ../Compare_ID/compare.py $NAME
+python ./Add_new_ID/add_ID.py $NAME
+python ./Compare_ID/compare.py $NAME
 
 # DELETE IMAGES IN RPi
-'''
-DATE+=$'.jpg'
- Delete all files
-rm $DATE #remove black and white image
-DATE=${DATE%???}; # remove last three characters (_bw)
-DATE+=$'.jpg'
-rm $DATE #remove cropped image
-DATE=${DATE%????????????}; # remove last 8+4  characters (_cropped.jpg)
-DATE+=$'.jpg'
-rm $DATE #remove original image
-'''
+
+#DATE+=$'.jpg'
+# Delete all files
+#rm $DATE #remove black and white image
+#DATE=${DATE%???}; # remove last three characters (_bw)
+#DATE+=$'.jpg'
+#rm $DATE #remove cropped image
+#DATE=${DATE%????????????}; # remove last 8+4  characters (_cropped.jpg)
+#DATE+=$'.jpg'
+#rm $DATE #remove original image

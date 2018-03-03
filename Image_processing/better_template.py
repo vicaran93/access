b60098327e7637117e3668ddb62d28a67957c7f1
Hassaan <hqkhan@umass.edu>
2018-03-02 19:10:50 -0500

    center name

--- Image_processing/better_template.py
+++ Image_processing/better_template.py
@@ -71,7 +71,7 @@ def main():
     template = Image.fromarray(template)
     template.save(path+"template.jpg")
     
-    with open('/home/pi/Documents/access/camera/location.txt' ,'w') as my_file:
+    with open('/home/pi/Documents/access/camera/%d_%d.txt'%(cent[0],cent[1]) ,'w') as my_file:
         my_file.write('%d %d\n'%(cent[0], cent[1]))
     
 if __name__ == "__main__":

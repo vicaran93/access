c133639f5ffa8f2f593008af5a62b2bb7147d78f
Hassaan <hqkhan@umass.edu>
2018-03-04 23:52:57 -0500

    bugsss

--- LEDs/LED_and_pic2.py
+++ LEDs/LED_and_pic2.py
@@ -55,7 +55,7 @@ def LED_and_img():
     b_w_img_filtered.save(bw_img_path)
     print("------------------------ Done ------------------------")
     
-    print b_w_img_filtered.shape
+    print np.array(b_w_img_filtered).shape
     template, cent = cn.grid_image_template(np.array(b_w_img_filtered)) #I_np)
     print 'gridded the template'
     

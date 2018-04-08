0e32e521f6f7fdfcdc691666b362728af30fd478
Victor Arango <varangoquiro@umass.edu>
2018-04-08 17:46:04 -0400

    add template as well

--- Add_new_ID/upload_RPi.py
+++ Add_new_ID/upload_RPi.py
@@ -50,12 +50,17 @@ except botocore.exceptions.ClientError as e:
 
 # Bucket is opened. Now we can upload file to Bucket
 
+s3 = boto3.client('s3')
+with open(file_path, 'rb') as data:
+    s3.upload_file(
+        file_path, aws_s3_bucket_name, file_name , # file_name --> 'folder/{}'.format(filename)
+        ExtraArgs={'ACL': 'public-read'}
+    )
 # Upload image:
 if len(sys.argv) == 3: # two inputs
     file_name = file_name+template_number+".jpg"
-else:
-    file_name = file_name + ".jpg"
-s3 = boto3.client('s3')
+
+
 with open(file_path, 'rb') as data:
     s3.upload_file(
         file_path, aws_s3_bucket_name, '9004/{}'.format(file_name) , # file_name --> 'folder/{}'.format(filename)
